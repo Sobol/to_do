d58b30dfd337b97a6115b99a243c4857851f3ff8
Paweł Sobolewski <pawel.sobolewski@amberbit.com>
2013-06-24 18:47:16 +0200

    Created repo

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+ruby '2.0.0'
+
+gem 'pg'
+gem 'will_paginate'
+
+group :development do
+  gem 'guard-livereload', require: false
+end
+
+group :test, :development do
+  gem 'rspec-rails', require: false
+end
+
+group :test do
+  gem 'capybara', '~> 2.0.0'
+  gem 'selenium-webdriver'
+  gem 'capybara-firebug'
+  gem 'poltergeist'
+  gem 'faker'
+  gem 'database_cleaner'
+end
+
+gem "jquery-rails", "2.3.0"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToDo::Application.config.secret_token = '9163350802ffc70ad6cbc9096ec7c854146759915a9675890f4d78b23f40b1ee1288afc0e87942b9c5c3a920770d320eb7aacf12d17b0b01b7d76bf84c17c592'
